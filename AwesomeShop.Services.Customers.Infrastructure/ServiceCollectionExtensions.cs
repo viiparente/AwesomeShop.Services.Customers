c7966ec342498d4fe8e1fd199ea041edd8ea0450
Vinicius Parente <vd.parente@hotmail.com>
2022-02-21 21:29:41 -0300

    Implement data access with MongoDB

--- AwesomeShop.Services.Customers.Infrastructure/ServiceCollectionExtensions.cs
+++ AwesomeShop.Services.Customers.Infrastructure/ServiceCollectionExtensions.cs
@@ -1,8 +1,63 @@
-﻿using System;
+﻿using AwesomeShop.Services.Customers.Core.Entities;
+using AwesomeShop.Services.Customers.Core.Repositories;
+using AwesomeShop.Services.Customers.Infrastructure.Persistence;
+using AwesomeShop.Services.Customers.Infrastructure.Persistence.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
 
 namespace AwesomeShop.Services.Customers.Infrastructure
 {
-    public class ServiceCollectionExtensions
+    public static class ServiceCollectionExtensions
     {
+        public static IServiceCollection AddMongo(this IServiceCollection services)
+        {
+            services.AddSingleton(sp => {
+                var configuration = sp.GetService<IConfiguration>();
+                var options = new MongoDbOptions();
+
+                configuration.GetSection("Mongo").Bind(options);
+
+                return options;
+            });
+
+            services.AddSingleton<IMongoClient>(sp => {
+                var options = sp.GetService<MongoDbOptions>();
+                return new MongoClient(options.ConnectionString);
+            });
+
+            services.AddTransient(sp => {
+                BsonDefaults.GuidRepresentation = GuidRepresentation.Standard;
+
+                var options = sp.GetService<MongoDbOptions>();
+                var mongoClient = sp.GetService<IMongoClient>();
+
+                return mongoClient.GetDatabase(options.Database);
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddMongoRepository<Customer>("customers");
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+
+            return services;
+        }
+
+        private static IServiceCollection AddMongoRepository<T>(this IServiceCollection services, string collection) where T : IEntityBase
+        {
+            services.AddScoped<IMongoRepository<T>>(f =>
+            {
+                var mongoDatabase = f.GetRequiredService<IMongoDatabase>();
+
+                return new MongoRepository<T>(mongoDatabase, collection);
+            });
+
+            return services;
+        }
     }
 }
